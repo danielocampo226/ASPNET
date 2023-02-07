a477094be50818109830d2bd51ea9eef77ddb969
danielocampo226 <sierradanny562@gmail.com>
2023-02-06 19:15:20 -0500

    ASP.NET

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
